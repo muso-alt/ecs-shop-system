f81d6f46c11202156f8c66d99dad0d8dd232325b
Мусо Исмаилов <musoismailov16@gmail.com>
2023-10-30 23:28:35 +0500

    Base update

---
+++ Assets/Scripts/EcsStartup.cs
@@ -0,0 +1,18 @@
+using Leopotam.EcsLite;
+using UnityEngine;
+
+public class EcsStartup : MonoBehaviour
+{
+    private EcsWorld _world;
+    private IEcsSystems _systems;
+    
+    private void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+}
