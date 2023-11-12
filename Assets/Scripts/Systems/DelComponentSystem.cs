3f20ac0175cc32ffdc9bdc831451e97c2112f646
Мусо Исмаилов <musoismailov16@gmail.com>
2023-11-13 00:32:06 +0500

    Click tools components added

---
+++ Assets/Scripts/Systems/DelComponentSystem.cs
@@ -0,0 +1,18 @@
+﻿using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+
+namespace ShopComplex.Systems
+{
+    public class DelComponentSystem<T> : IEcsRunSystem where T : struct
+    {
+        private EcsFilterInject<Inc<T>> _filter;
+        
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var entity in _filter.Value)
+            {
+                _filter.Pools.Inc1.Del(entity);
+            }
+        }
+    }
+}
